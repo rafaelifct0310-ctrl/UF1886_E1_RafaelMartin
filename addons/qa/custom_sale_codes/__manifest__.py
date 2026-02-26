7bf72103e78c57cb93ea15f36226e6a25e85a52a
rafael <rafael@gmail.com>
2026-02-26 10:15:01 +0000

    actualización qa

---
+++ addons/qa/custom_sale_codes/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Custom Sale Codes (Report)",
+    "version": "1.0.0",
+    "category": "Sales",
+    "summary": "Añade referencia interna y código de barras al reporte de pedido.",
+    "depends": ["sale"],
+    "data": [
+        "views/sale_report.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3"
+}
